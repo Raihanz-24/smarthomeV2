5ce6c9676f108fc54e549d6101569fa503ff6890
Raihan <ramaraihan559@gmail.com>
2024-11-25 22:24:41 +0700

    d

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ document.getElementById("relayOn").addEventListener("click", () => {
 document.getElementById("relayOff").addEventListener("click", () => {
   fetch("https://api.telegram.org/bot7938826396:AAGqbyvm4a9KdebQkCOGXXRrqHwDc2zBshk/sendMessage?chat_id=1002265746909&text=OFF");
 });
+
+let deferredPrompt;
+const installButton = document.getElementById('install-button');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
